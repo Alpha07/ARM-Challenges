c51ed768eca92853b3d893696870a5e9edaa4449
Serafi Nebot Ginard <serafinebot@Serafi-Nebot.local>
2018-01-01 19:47:11 +0100

    Added initial files

---
+++ stack0/stack0.c
@@ -0,0 +1,30 @@
+
+// stack0.c
+// Created by Snebot26
+
+/*
+ * compile: $(xcrun --sdk iphoneos --find clang) -isysroot $(xcrun --sdk iphoneos --show-sdk-path) --target=<target> -fno-stack-protector stack0.c -o <build path>/stack0
+ * Examples for <target> -> armv7-apple-darwin11, arm64-apple-darwin
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    printf("Welcome to stack0! Your goal here is to modify a variable's value, good luck!\n\nGive me something: ");
+    
+    int value = 0;              // Variable we want to check
+    char *buffer[16];           // Variable where we will store user's input
+    gets((char *)buffer);       // Vulnerable function
+    
+    // Check for value's value
+    if (value != 0) {
+        
+        printf("You have succesfully changed the variable 'value'!\n");     // Succes message
+    } else {
+        
+        printf("Keep trying!\n");       // Fail message
+    }
+    
+    return 0;
+}
